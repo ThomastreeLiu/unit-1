374713393cee854b2402dae73074ca2ea6e02f4b
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-09-11 13:45:50 -0700

    test.java now renamed to HelloWorld.java //owl.java finish

--- src/Umbrella.java
+++
@@ -1,9 +0,0 @@
-public class Umbrella {
-    public static void main (String args[]){
-        System.out.println(" .-^-. " +
-                "\n" +
-                "'\"'|`\"`" +
-                "\n" +
-                "   j ");
-    }
-}
