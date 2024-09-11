84a5cd3fa1fd4c9e8c81e7854e73bf58dff0a163
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-09-11 10:47:37 -0700

    finally a new file is working now

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,20 @@ class Favourites{
     public static void main(String args[]){
 
         System.out.println("My name is Thomas");
-        System.out.println("is it working?");
-
+        System.out.println("green");
+        System.out.println("CCTV-7");
+        System.out.println("");
+        System.out.println("WarThunder");
+        System.out.println("Dumplings");
+        System.out.println("Micky");
+        System.out.println("Alan Walker");
+        System.out.println("banminton");
+        System.out.println("");
+        System.out.println("science");
+        System.out.println("Los anglelas");
+        System.out.println("1 little brother");
+        System.out.println("I want to learn programming");
+        System.out.println("");
     }
 
 }
