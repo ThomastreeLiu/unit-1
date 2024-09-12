637862f34ed2e4e9301be4f24a22c3afa45d9445
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-09-12 13:55:23 -0700

    Output.java upload

--- src/Output.java
+++ src/Output.java
@@ -6,21 +6,59 @@ public class Output {
         Scanner input = new Scanner(System.in);
 
 
-        System.out.print ("What is your favourite number? ");
+        System.out.print ("What's your first name? ");
 
         String num = input.nextLine();
 
         System.out.println ("You answered: " + num);
 
         System.out.println ("");
+        //第一段
 
-        System.out.print ("What did you eat for breakfast? ");
+        System.out.print ("What's your last name? ");
 
         String breakfast = input.nextLine();
 
         System.out.println ("You answered: " + breakfast);
 
         System.out.println ("");
+        //第二段
+
+        System.out.print ("What's your age? ");
+
+        String num = input.nextLine();
+
+        System.out.println ("You answered: " + num);
+
+        System.out.println ("");
+        //第三段
+
+        System.out.print ("What is your postal number? ");
+
+        String num = input.nextLine();
+
+        System.out.println ("You answered: " + num);
+
+        System.out.println ("");
+        //第四段
+
+        System.out.print ("What is the year you born ");
+
+        String num = input.nextLine();
+
+        System.out.println ("You answered: " + num);
+
+        System.out.println ("");
+        //第五段
+
+        System.out.print ("What is your school attending ");
+
+        String num = input.nextLine();
+
+        System.out.println ("You answered: " + num);
+
+        System.out.println ("");
+        //第六段
 
     }
 }
