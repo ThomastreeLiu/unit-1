2195a7091b466a5a6c3f1cbd8c8d8115e78413fc
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-09-13 18:26:41 -0700

    Output.java upload

--- src/2024.9.11/BookCover.java
+++ src/2024.9.11/BookCover.java
@@ -3,15 +3,15 @@ public class BookCover {
         public static void main (String args[]){
             System.out.println("By Dr. Seuss\n"+"One Fish" +
                     "\n" +
-                    "<)))>< \n" +
+                    "<)))>< " +
                     "\n" +
                     "Two Fish" +
                     "\n" +
-                    "><)))> \n" +
+                    "><)))> " +
                     "\n" +
                     "Red Fish" +
                     "\n" +
-                    "<)))>< \n" +
+                    "<)))>< " +
                     "\n" +
                     "Blue Fish" +
                     "\n" +
