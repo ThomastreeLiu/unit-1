301083a7a98283f1d6091479983580af57330039
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-09-18 13:21:28 -0700

    Output.java upload

---
+++ src/test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class test {
+    public static void main (String args []){
+
+        Scanner scan = new Scanner(System.in);
+        double alpha = scan.nextDouble();
+        double beta = alpha*0.5;
+        System.out.println(beta);
+
+
+    }
+}
