9fced1cf2750f6712777379f1ea73dd5c94415a3
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-09-19 10:11:03 -0700

    triangle update

---
+++ src/circle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class circle {
+    public static void main (String args []){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Calculate the area & circumference of a circle\n"+"Now input the radius of the circle\n");
+        double radius = scan.nextDouble();
+        double area = Math.pow(radius,2)*Math.PI;
+        double circumference = 
+        System.out.println(area);
+
+
+    }
+}
