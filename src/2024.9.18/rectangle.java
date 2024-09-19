0bf9c022980208974288476720448a64b5725b5b
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-09-19 10:54:06 -0700

    circle.java upload

--- src/2024.9.18/rectangle.java
+++ src/2024.9.18/rectangle.java
@@ -5,16 +5,12 @@ public class rectangle {
             System.out.println("Calculate the area & perimeter\n"+"Now input the length in meter\n");
             Scanner scan = new Scanner(System.in);
             double length = scan.nextDouble();
-            //double beta = length *0.5;
-            //System.out.println(beta);
             System.out.println("Now input the width in meter\n");
             double width = scan.nextDouble();
             double area = length*width;
             double perimeter = 2*length+2*width;
             System.out.println("The area of this rectangle is "+area+" square meters.\n");
             System.out.println("The perimeter of this rectangle is "+perimeter+" meters.");
-
-
         }
     }
 
--- src/trapezoid.java
+++ src/trapezoid.java
@@ -6,8 +6,14 @@ public class trapezoid {
         Scanner scan = new Scanner(System.in);
         System.out.println("Calculate the area & perimeter of a trapezoid.\n"+ "watch the graph showed under\n     a");
         System.out.println("   ________\n"+"c /  h |   \\ d\n"+" /_____|____\\\n"+"      b");
-        System.out.println("Now input the a in meter");
-        //double radius = scan.nextDouble();
+        System.out.println("Now input the a in meters");
+        double a = scan.nextDouble();
+        System.out.println("Now input the b in meters");
+        double b = scan.nextDouble();
+        System.out.println("Now input the c in meters");
+        double c = scan.nextDouble();
+        System.out.println("Now input the d in meters");
+        double d = scan.nextDouble();
         //double area = Math.pow(radius,2)*Math.PI;
         //double circumference = radius*Math.PI*2;
         //System.out.println("Area of the circle is "+area+"\nCircumference of the circle is "+circumference);
