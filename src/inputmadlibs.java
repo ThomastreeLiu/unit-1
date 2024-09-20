95e4885b66c181755d59d861738eb394305bd70a
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-09-20 10:33:14 -0700

    circle.java upload

---
+++ src/inputmadlibs.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class inputmadlibs {
+    public static void main (String args []){
+
+        Scanner scan = new Scanner(System.in);
+
+//this is just an example to help you understand what is required
+
+        System.out.println("Enter a adjective: ");
+
+        String adj = scan.nextLine();
+
+        System.out.println("Enter a number: ");
+
+        String num = scan.nextLine();
+
+        System.out.println("Enter a : ");
+
+        String col2 = scan.nextLine();
+
+        System.out.println("Enter a number: ");
+
+        String num = scan.nextLine();
+
+        System.out.println("Enter another number: ");
+
+        String num2 = scan.nextLine();
+
+
+        System.out.println("");
+
+        System.out.println("A Strange Animal");
+
+        System.out.println("The "+madeup+" is an animal that has "+col+" fur with "+col2); System.out.println("spots on its tail. An adult "+madeup+" may weigh over "+num); System.out.println("pounds and stand over "+num2+" feet tall.");
+
+    }
+}
