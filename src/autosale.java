d1344ec34f720ecdaeb0dcfb834a78abaeb671a4
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-09-24 11:29:49 -0700

    autosale.java upload inputmadlibs.java need to be change

--- src/autosale.java
+++ src/autosale.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.text.NumberFormat;
 public class autosale {
     public static void main (String args []){
         Scanner scan = new Scanner(System.in);
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
         System.out.println("********************************* \n" +
                 "*    Welcome to My Farm Stall   * \n" +
                 "********************************* ");
@@ -20,16 +22,19 @@ public class autosale {
         System.out.println("**************************** \n" +
                 "*        Food Bill        * \n" +
                 "*************************** ");
-        System.out.println(strw+" strawberry @ $5 = $"+5*strw);
-        System.out.println(egg+" eggplant @ $2 = $"+2*egg);
-        System.out.println(flow+" flower @ $1 = $"+1*flow);
-        System.out.println(pump+" pumpkin @ $2 = $"+2*pump);
-        System.out.println(strw+" soybean @ $3 gold = $"+5*syb);
+        System.out.println(strw+" strawberry @ $5 = "+currency.format(5*strw));
+        System.out.println(egg+" eggplant @ $2 = "+currency.format(2*egg));
+        System.out.println(flow+" flower @ $1 = "+currency.format(1*flow));
+        System.out.println(pump+" pumpkin @ $2 = "+currency.format(2*pump));
+        System.out.println(strw+" soybean @ $3 gold = "+currency.format(3*syb));
 
         int sto = 5*strw+2*egg+1*flow+2*pump+5*syb;
         double tax = sto*0.12;
         double tot =sto+tax;
-        System.out.println("Subtotal: $"+sto+"\nTax: $"+tax+"\nTotal: $"+tot);
+        System.out.println("Subtotal: "+currency.format(sto)+"\nTax: "+currency.format(tax)+"\nTotal: "+currency.format(tot));
+        System.out.println("Enter the amount of money paid: $");
+        double paid = scan.nextDouble();
+        System.out.println("Your charge is "+currency.format(paid-tot));
 
 
     }
--- src/inputmadlibs.java
+++ src/inputmadlibs.java
@@ -10,18 +10,18 @@ public class inputmadlibs {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter a adjective. ");
-        String I = scan.nextLine();
+        String word1 = scan.nextLine();
 
         System.out.println("Enter a number. ");
         String II = scan.nextLine();
 
-        String IP = ("There're a lot of "+I+" ways to get to school. " +
+        String IP = ("There're a lot of "+word1+" ways to get to school. " +
                 "Here's a list of the top "+II+".");
         //introduction out
 
 
         System.out.println("Enter a type of building. ");
-        I = scan.nextLine();
+        word1 = scan.nextLine();
 
         System.out.println("Enter a type of food. ");
         II = scan.nextLine();
@@ -32,22 +32,22 @@ public class inputmadlibs {
         System.out.println("Enter a verb.");
         String IV = scan.nextLine();
 
-        String IIP = ("The "+I+" bus:Watch out for "+II+" " +
+        String IIP = ("The "+word1+" bus:Watch out for "+II+" " +
                 "stuck under the seats and hold onto your hardcover "+III+
                 " in case the driver has to "+IV+" the brakes.");
         //paragraph 1 out
 
         System.out.println("Enter a adjective.");
-        I = scan.nextLine();
+        word1 = scan.nextLine();
 
         System.out.println("Enter a verb ending in ing.");
         II = scan.nextLine();
         String IIIP = ("Bicycle:Check the weather, because it's "+
-                I+" to hold an umbrella while "+II+".");
+                word1+" to hold an umbrella while "+II+".");
         //paragraph 2 out
 
         System.out.println("Enter a verb. ");
-        I = scan.nextLine();
+        word1 = scan.nextLine();
 
         System.out.println("Enter a animal. ");
         II = scan.nextLine();
@@ -55,12 +55,12 @@ public class inputmadlibs {
         System.out.println("Enter a adjective. ");
         III = scan.nextLine();
 
-        String IVP = ("Walk:Don't stop to "+I+" every "+II+" along the way" +
+        String IVP = ("Walk:Don't stop to "+word1+" every "+II+" along the way" +
                 " or you'll be "+III+" for school!");
         //paragraph 3 out
 
         System.out.println("Enter a noun. ");
-        I = scan.nextLine();
+        word1 = scan.nextLine();
 
         System.out.println("Enter a occupation. ");
         II = scan.nextLine();
@@ -73,7 +73,7 @@ public class inputmadlibs {
         System.out.println("1. "+IIP);
         System.out.println("2. "+IIIP);
         System.out.println("3. "+IVP);
-        IVP = ("Skate-"+I+": Cool, but if the "+II+" catches you doing " +
+        IVP = ("Skate-"+word1+": Cool, but if the "+II+" catches you doing " +
                 "jumps off the school's front "+III+" , you're getting detention!");
         //paragraph 5 out
         System.out.println("4. "+IVP);
