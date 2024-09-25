4a70e46c82f837fc647d791875f7a3c11b221df1
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-09-25 11:13:45 -0700

    Eggcalculate.java complete

---
+++ src/Eggcalculate.java
@@ -0,0 +1,18 @@
+//Author:Thomas
+//Name:Autosale
+//purpose:make a bill automatically
+
+import java.util.Scanner;
+public class Eggcalculate {
+    public static void main (String args []){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("How many eggs?");
+        int egg = scan.nextInt();
+        int crate = egg/180;
+        egg = egg%180;
+        int carton = egg/12;
+        egg = egg%12;
+        System.out.println("That's "+crate+" crate(s), "+carton+" carton(s) ,and "+egg+"(s) left over.");
+    }
+}
