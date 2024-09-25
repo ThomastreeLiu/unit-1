39f8e25bdd607061cdf27f20b136b2858dc9fd56
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-09-25 10:52:16 -0700

    autosale.java complete inputmadlibs.java complete

--- src/Autosale.java
+++ src/Autosale.java
@@ -4,7 +4,7 @@
 
 import java.util.Scanner;
 import java.text.NumberFormat;
-public class autosale {
+public class Autosale {
     public static void main (String args []){
         Scanner scan = new Scanner(System.in);
         NumberFormat currency = NumberFormat.getCurrencyInstance();
@@ -17,24 +17,24 @@ public class autosale {
         System.out.println("How many eggplants would you like?");
         int egg = scan.nextInt();
         System.out.println("How many flower would you like?");
-        int flow = scan.nextInt();
+        int flower = scan.nextInt();
         System.out.println("How many pumpkins would you like?");
         int pump = scan.nextInt();
         System.out.println("How many soybeans would you like?");
-        int syb = scan.nextInt();
+        int soybean = scan.nextInt();
 
         System.out.println("**************************** \n" +
                 "*        Food Bill        * \n" +
                 "*************************** ");
         System.out.println(strw+" strawberry @ $5 = "+currency.format(5*strw));
         System.out.println(egg+" eggplant @ $2 = "+currency.format(2*egg));
-        System.out.println(flow+" flower @ $1 = "+currency.format(1*flow));
+        System.out.println(flower+" flower @ $1 = "+currency.format(1*flower));
         System.out.println(pump+" pumpkin @ $2 = "+currency.format(2*pump));
-        System.out.println(strw+" soybean @ $3 gold = "+currency.format(3*syb));
-        int sto = 5*strw+2*egg+1*flow+2*pump+5*syb;
-        double tax = sto*0.12;
-        double tot =sto+tax;
-        System.out.println("Subtotal: "+currency.format(sto)+"\nTax: "+currency.format(tax)+"\nTotal: "+currency.format(tot));
+        System.out.println(strw+" soybean @ $3 gold = "+currency.format(3*soybean));
+        int subtot = 5*strw+2*egg+1*flower+2*pump+5*soybean;
+        double tax = subtot*0.12;
+        double tot =subtot+tax;
+        System.out.println("Subtotal: "+currency.format(subtot)+"\nTax: "+currency.format(tax)+"\nTotal: "+currency.format(tot));
         System.out.println("Enter the amount of money paid: $");
         double paid = scan.nextDouble();
         System.out.println("Your charge is "+currency.format(paid-tot));
