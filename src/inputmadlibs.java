b2dc36c3fbf9142bc8c496a0afb009ba0ea6bbfa
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-09-25 10:28:46 -0700

    autosale.java complete inputmadlibs.java complete

--- src/inputmadlibs.java
+++ src/inputmadlibs.java
@@ -13,10 +13,10 @@ public class inputmadlibs {
         String word1 = scan.nextLine();
 
         System.out.println("Enter a number. ");
-        String II = scan.nextLine();
+        String word2 = scan.nextLine();
 
-        String IP = ("There're a lot of "+word1+" ways to get to school. " +
-                "Here's a list of the top "+II+".");
+        String paragraph1 = ("There're a lot of "+word1+" ways to get to school. " +
+                "Here's a list of the top "+word2+".");
         //introduction out
 
 
@@ -24,59 +24,59 @@ public class inputmadlibs {
         word1 = scan.nextLine();
 
         System.out.println("Enter a type of food. ");
-        II = scan.nextLine();
+        word2 = scan.nextLine();
 
         System.out.println("Enter a plural noun. ");
-        String III = scan.nextLine();
+        String word3 = scan.nextLine();
 
         System.out.println("Enter a verb.");
-        String IV = scan.nextLine();
+        String word4 = scan.nextLine();
 
-        String IIP = ("The "+word1+" bus:Watch out for "+II+" " +
-                "stuck under the seats and hold onto your hardcover "+III+
-                " in case the driver has to "+IV+" the brakes.");
+        String paragraph2 = ("The "+word1+" bus:Watch out for "+word2+" " +
+                "stuck under the seats and hold onto your hardcover "+word3+
+                " in case the driver has to "+word4+" the brakes.");
         //paragraph 1 out
 
         System.out.println("Enter a adjective.");
         word1 = scan.nextLine();
 
         System.out.println("Enter a verb ending in ing.");
-        II = scan.nextLine();
-        String IIIP = ("Bicycle:Check the weather, because it's "+
-                word1+" to hold an umbrella while "+II+".");
+        word2 = scan.nextLine();
+        String paragraph3 = ("Bicycle:Check the weather, because it's "+
+                word1+" to hold an umbrella while "+word2+".");
         //paragraph 2 out
 
         System.out.println("Enter a verb. ");
         word1 = scan.nextLine();
 
         System.out.println("Enter a animal. ");
-        II = scan.nextLine();
+        word2 = scan.nextLine();
 
         System.out.println("Enter a adjective. ");
-        III = scan.nextLine();
+        word3 = scan.nextLine();
 
-        String IVP = ("Walk:Don't stop to "+word1+" every "+II+" along the way" +
-                " or you'll be "+III+" for school!");
+        String paragraph4 = ("Walk:Don't stop to "+word1+" every "+word2+" along the way" +
+                " or you'll be "+word3+" for school!");
         //paragraph 3 out
 
         System.out.println("Enter a noun. ");
         word1 = scan.nextLine();
 
         System.out.println("Enter a occupation. ");
-        II = scan.nextLine();
+        word2 = scan.nextLine();
 
         System.out.println("Enter a plural noun. ");
-        III = scan.nextLine();
+        word3 = scan.nextLine();
         //paragraph 4 out
 
-        System.out.println(IP);
-        System.out.println("1. "+IIP);
-        System.out.println("2. "+IIIP);
-        System.out.println("3. "+IVP);
-        IVP = ("Skate-"+word1+": Cool, but if the "+II+" catches you doing " +
-                "jumps off the school's front "+III+" , you're getting detention!");
+        System.out.println(paragraph1);
+        System.out.println("1. "+paragraph2);
+        System.out.println("2. "+paragraph3);
+        System.out.println("3. "+paragraph4);
+        paragraph4 = ("Skate-"+word1+": Cool, but if the "+word2+" catches you doing " +
+                "jumps off the school's front "+word3+" , you're getting detention!");
         //paragraph 5 out
-        System.out.println("4. "+IVP);
+        System.out.println("4. "+paragraph4);
     //output finish
     }
 }
