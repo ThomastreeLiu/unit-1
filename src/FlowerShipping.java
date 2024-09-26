3b5f7f5eb158ad5de5aabd4f96f504e5d5326a18
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-09-26 10:56:40 -0700

    FlowerShipping.java complete

--- src/FlowerShipping.java
+++ src/FlowerShipping.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class FlowerShipping {
     public static void main (String args []){
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Enter the number of flower pots");
         int potnum = scan.nextInt();
         int numbox = potnum/9;
@@ -14,6 +15,7 @@ public class FlowerShipping {
         String ans = numbox+" Very big Boxes,\n";
         numbox = potnum/4;
         potnum = potnum%4;
+
         total = total+numbox+potnum;
         ans = ans+numbox+" Big Boxes,\n"+potnum+" Small Boxes,\n";
         System.out.println(ans+total+" Total");
