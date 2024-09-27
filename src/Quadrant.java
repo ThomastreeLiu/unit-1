0468a99d10c2d5c10a17ab7ffe03ee033df71c40
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-09-27 10:44:08 -0700

    Quadrant.java complete

---
+++ src/Quadrant.java
@@ -0,0 +1,33 @@
+//Author:Thomas
+//Name:Quadrant
+//Purpose:enter a point, print a quadrant
+
+import java.util.Scanner;
+public class Quadrant {
+    public static void main (String args []) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a point, in x,y.\nEnter x");
+        double x = scan.nextDouble();
+        System.out.println("Enter y");
+        double y = scan.nextDouble();
+        if (x > 0) {
+            if (y > 0)
+                System.out.println("This point located at quadrant 1.");
+            else if (y < 0)
+                System.out.println("This point located at quadrant 4");
+            else
+                System.out.println("This point is not belongs to any quadrant");
+        }
+        else if (x <0) {
+            if (y >0)
+                System.out.println("This point located at quadrant 2");
+            else if (y < 0)
+                System.out.println("This point locates at quadrant 3");
+            else
+                System.out.println("This point is not belongs to any quadrant");
+        }
+        else
+            System.out.println("This point is not belongs to any quadrant");
+
+    }
+}
