98b361c96483e1f3a4d3f45758af2b7b085f650c
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-10-01 13:57:08 -0700

    Cashregister.java update

--- src/Cashregister.java
+++ src/Cashregister.java
@@ -4,37 +4,45 @@
 
 import java.util.Scanner;
 public class Cashregister {
-    public static void main (String args []){
+    public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter the cash in cent.");
         int cash = scan.nextInt();
 
         if (cash >= 2000) {
-            System.out.println("$20 bills: "+cash/2000);
-            cash = cash%2000;
+            System.out.println("$20 bills: " + cash/2000);
+            cash = cash % 2000;
         } else if (cash >= 1000) {
-            System.out.println("$10 bills:"+cash/1000);
-            cash = cash%1000;
+            System.out.println("$10 bills:" + cash/1000);
+            cash = cash % 1000;
         } else if (cash >= 500) {
-            System.out.println("$5 bills:"+cash/500);
-            cash = cash%500;
+            System.out.println("$5 bills:" + cash/500);
+            cash = cash % 500;
         }
-        /////
-        int twoonies = cash/ ;
-        cash = cash% ;
-        int loonies = cash
 
-                //////
-
+        //int twoonies = cash/ ;
+        //cash = cash% ;
+        //int loonies = cash/ ;
+        //cash = cash% ;
 
         int quarters = cash/25;
-        cash = cash%25;
+        cash = cash % 25;
         int dimes = cash/25;
         cash = cash%10;
         int nickel = cash/5;
 
+        if (quarters > 0) {
+            System.out.println("Quarters:" + quarters);
+        }
+        if (dimes > 0) {
+            System.out.println("Dimes:"+dimes);
+        }
+        if (cash > 0){
+            System.out.println("Pennies:"+cash);
+        }
 
 
     }
+
 }
