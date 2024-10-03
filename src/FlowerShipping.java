2cd46f6c8e3077d97367d2c4f10cb992aede406f
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-10-03 13:30:48 -0700

    FlowerShipping.java complete

--- src/FlowerShipping.java
+++ src/FlowerShipping.java
@@ -8,19 +8,18 @@ public class FlowerShipping {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter the number of flower pots");
-        int potnum = scan.nextInt();
-        int numbox = potnum/9;
-        potnum = potnum%9;
+        int numPots = scan.nextInt();
+        int numVeryBig = numPots/9;
+        numPots = numPots%9;
 
-        int total = numbox;
-        String ans = numbox+" Very big Boxes,\n";
+        int total = numVeryBig;
 
-        numbox = potnum/4;
-        potnum = potnum%4;
+        int numBig = numPots/4;
+        numPots = numPots%4;
 
-        total = total+numbox+potnum;
-        ans = ans+numbox+" Big Boxes,\n"+potnum+" Small Boxes,\n";
-        System.out.println(ans+total+" Total");
+        total = total+numBig+numPots;
+        System.out.println(numVeryBig+" Very Big Box,\n"+numBig+" Big Box,");
+        System.out.println(numPots+" Small Box,\n"+total+" Total");
 
     }
 }
