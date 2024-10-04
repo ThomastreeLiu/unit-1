9c8f1095b31922ea16c1a9ed6d75c8c867674e13
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-10-04 10:59:56 -0700

    SeriesTask.java complete

---
+++ src/SeriesTask1.java
@@ -0,0 +1,33 @@
+public class SeriesTask1 {
+    public static void main(String[] args) {
+        for (int i=0;i<26;i++){
+            System.out.print(i+" ");
+        }
+        System.out.println(" \n");
+
+        for (int i=5;i<19;i++){
+            System.out.print(i+" ");
+        }
+        System.out.println(" \n");
+
+        for (int i=30;i<51;i++){
+            System.out.print(i+" ");
+        }
+        System.out.println(" \n");
+
+        for (int i=12;i>0;i--){
+            System.out.print(i+" ");
+        }
+        System.out.println(" \n");
+
+        for (int i=100;i>84;i--){
+            System.out.print(i+" ");
+        }
+        System.out.println(" \n");
+
+        for (int i=5;i<71;i=i+5){
+            System.out.print(i+" ");
+        }
+        System.out.println(" ");
+    }
+}
