f523d4eff6aed444a563bf3baa787bcacdc0bcdb
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-10-04 10:18:48 -0700

    FlowerShipping.java complete

--- src/FlowerShipping.java
+++ src/FlowerShipping.java
@@ -4,7 +4,7 @@
 
 import java.util.Scanner;
 public class FlowerShipping {
-    public static void main (String args []){
+    public static void main (String[] args){
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter the number of flower pots");
